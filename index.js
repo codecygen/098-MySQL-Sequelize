0a23495be164bbaa36cada15aad2220be5d39152
Aras <vahitaras@gmail.com>
2023-03-26 15:55:09 -0400

    extra line of explanation added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 
 const { sequelize, databaseAuth } = require("./models/dbConnection");
+
+// Needs to be important even if it is not used
+// Because it is seen by sequelize.sync method in this file
+// and the model is updated accordingly.
 const User = require("./models/userModel");
 
 databaseAuth();
