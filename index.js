ebcd2ee3e7e33a93eba12831adbb6ad1924eb64a
Aras <vahitaras@gmail.com>
2023-03-27 17:16:21 -0400

    add some useful notes about sequelize methods that can be used in different situations.

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ const Post = require("./models/postModel");
 
 databaseAuth();
 
+
+// User.sync.then.catch, // would only sync User model.
+// User.drop(); // would drop the table and if we write it would not exist.
+// sequelize.drop({ match: /^test/ }); // drop tables that starts with "test"
+// sequelize.drop({ match: /test$/ }); // drop tables that ends with "test"
+// sequelize.drop({ match: /aras/i }); // drop tables that has "aras" in it, looks for the string in a case insensitive way.
+// sequelize.drop({ match: /g/g }); // drop tables that has letter "g" in it.
+// sequelize.drop({ match: /g/ }); // find the first occurence of letter g in a tables and drop them.
+// sequelize.drop({ match: /\d/g }); // finds all occurences of digits in tables and drop them.
+
 sequelize
-// force, deletes all data and recreates the database with the model configs,
-// alter, only alters the data structure in the database.
-// .sync({ force: true })
-  .sync({ alter: true })
+// .sync({ force: true }) // deletes all data and recreates the database with the model configs,
+  .sync({ alter: true }) // only alters the data structure in the database
   .then((result) => {
     console.log("Successfully created models!");
   })
--- models/dbConnection.js
+++ models/dbConnection.js
@@ -5,7 +5,8 @@ const {DB, SCHEMA, ADMIN, PASS, HOST, PORT} = process.env;
 const sequelize = new Sequelize(SCHEMA, ADMIN, PASS, {
   dialect: DB,
   host: HOST,
-  port: PORT,
+  port: PORT,    
+  //freezeTableName: true, // means in the next restarts, even if the model names get changed in the configs, do not change them in the database.
 });
 
 const databaseAuth = async () => {
