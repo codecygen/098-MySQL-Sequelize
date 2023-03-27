49a48c72c7778c25dff995bc78adccf0abaf9176
Aras <vahitaras@gmail.com>
2023-03-27 16:16:22 -0400

    updated note

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { sequelize, databaseAuth } = require("./models/dbConnection");
 
 // Needs to be imported even if it is not used
 // Because it is seen by sequelize.sync method in this file
-// and the model is updated accordingly.
+// and the model is updated accordingly in database.
 const User = require("./models/userModel");
 
 databaseAuth();
